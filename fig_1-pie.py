62b6c33021edf01281647cb40ad4ed889c5d4f22
Evgene-Kopylov <eugenevkopylov@gmail.com>
2023-03-25 09:21:12 +0200

    upd

--- fig_1-pie.py
+++ fig_1-pie.py
@@ -1,16 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(
-    "Задача 1 (Система управления товарами)/Агрегированные данные о вводе товаров в оборот с 2021-11-22 по "
-    "2022-11-21.csv")
 
-df.groupby(['operation_type']) \
-    .sum(numeric_only=False) \
-    .plot(kind='pie', y='cnt', autopct='%1.0f%%', figsize=(9, 9)) \
-    .yaxis.set_visible(False)
-plt.legend(labels=None, loc=4, fontsize=10)
-plt.savefig('figure.png')
+def make_a_pie(path: str = "Задача 1 (Система управления товарами)/Агрегированные данные о вводе товаров в оборот с 2021-11-22 по 2022-11-21.csv"):
+    """
+    рисует круглую диаграмму по данным о
+    Воде товаров в оборот
+    """
+
+    df = pd.read_csv(path)
+
+    df.groupby(['operation_type']) \
+        .sum(numeric_only=False) \
+        .plot(kind='pie', y='cnt', autopct='%1.0f%%', figsize=(9, 9)) \
+        .yaxis.set_visible(False)
+    plt.legend(labels=None, loc=4, fontsize=10)
+    plt.savefig('figure.png')
+
 
 if __name__ == "__main__":
-    print("start")
+    make_a_pie()
